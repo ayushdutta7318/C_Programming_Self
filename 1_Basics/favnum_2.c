6684f455534d66c1b0e46bd88e6a4d96cf3fd89c
ayushdutta7318 <ayushdutta7318@gmail.com>
2025-12-28 11:46:12 +0530

    Add files via upload

---
+++ 1_Basics/favnum_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int myFavNum;
+    printf("Enter fav num\n: ");
+    scanf("%d", &myFavNum);
+    printf("your fav num is %d\n", myFavNum);
+
+    return 0;
+}
